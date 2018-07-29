c35a1fd8facdce2ebcf047ba1c798c251b874956
bjimison <brady.t.jimison@gmail.com>
2018-07-29 14:59:07 -0700

    created app.js file where the food API Key is held for future AJAX calls

---
+++ fitness_app_project/fitness_app/static/js/app.js
@@ -0,0 +1,9 @@
+const food_api_id = 2d7d9644;
+const food_api_key = 8e911eeff3b68f04eafd1fffeaf16401;
+
+const food_url_base = 'https://api.edamam.com/api/food-database/parser';
+
+// sample url if a user searched for 'red apple'. the '%20' represents a space.
+// 'https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id={your app_id}&app_key={your app_key}'
+// this copy and paste the below URL into Postman if you want to have a look at the responses.
+// 'https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=2d7d9644&app_key=8e911eeff3b68f04eafd1fffeaf16401'
