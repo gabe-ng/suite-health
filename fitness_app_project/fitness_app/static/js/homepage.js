2f42ea5d16c5faf9ea66ceb89ad0ba03fd5f9612
bjimison <brady.t.jimison@gmail.com>
2018-07-30 15:27:29 -0700

    downloaded new files

---
+++ fitness_app_project/fitness_app/static/js/homepage.js
@@ -0,0 +1,39 @@
+const app_id = "&app_id=2d7d9644";
+const app_key = "&app_key=8e911eeff3b68f04eafd1fffeaf16401";
+// let food_params = '?ingr=' +
+
+// In the request header
+const wger_api_key = "3a799cb0dc24aa6c28713c85eb3f2be4eb6f48f0";
+// Authorization: Token 3a799cb0dc24aa6c28713c85eb3f2be4eb6f48f0
+
+const food_url_base = "https://api.edamam.com/api/food-database/parser";
+
+// sample url if a user searched for 'red apple'. the '%20' represents a space.
+// 'https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id={your app_id}&app_key={your app_key}'
+// this copy and paste the below URL into Postman if you want to have a look at the responses.
+// 'https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=2d7d9644&app_key=8e911eeff3b68f04eafd1fffeaf16401'
+
+// muscle endpoint
+// exercise category
+
+$("#food_btn").on("click", () => {
+  $.ajax({
+    type: "GET",
+    url:
+      "https://api.edamam.com/api/food-database/parser" +
+      food_params +
+      app_id +
+      app_key,
+    success: response => {
+      $("#food_feed").append(`
+            <ul>
+            <li>Food: ${response.hints.food.label}</li>
+            <li>Calories: ${response.hints.food.nutrients.kcal}</li>
+            <li>Protein: ${response.hints.food.nutrients.protein}</li>
+            <li>Fat: ${response.hints.food.nutrients.fat}</li>
+            <li>Carbs: ${response.hints.food.nutrients.carbs}</li>
+            </ul>
+            `);
+    }
+  });
+});
---
+++ fitness_app_project/fitness_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  ############## LOG IN ###############
+  path('', views.landing, name="landing"),
+  path('index/', views.index, name="index"),
+  path('login/', views.login_view, name="login"),
+  path('signup/', views.signup_view, name="signup"),
+  path('logout/', views.logout_view, name="logout"),
+  ############# HOMEPAGE ##############
+  path('homepage/', views.homepage, name="homepage"),
+  ############# PROFILE ###############
+  path('user/<username>/', views.dashboard, name='dashboard'),
+]
