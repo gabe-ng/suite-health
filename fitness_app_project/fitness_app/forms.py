9c3977bef78049f903668461a24dc5b3406d90ed
Riley <RP2@users.noreply.github.com>
2018-07-30 12:10:04 -0700

    login

--- fitness_app_project/fitness_app/forms.py
+++ fitness_app_project/fitness_app/forms.py
@@ -7,7 +7,10 @@ class LoginForm(forms.Form):
 class SignupForm(forms.Form):
     username = forms.CharField(label="User Name", max_length=64)
     password = forms.CharField(widget=forms.PasswordInput())
-    password = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+      fields = ['username', 'password', 'password2']
 
 
 # class CreateCircuitForm(forms.form):
