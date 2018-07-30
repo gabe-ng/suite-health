dd80ccb2140aaf4244de9d6dee81f5c81c5af6cd
bjimison <brady.t.jimison@gmail.com>
2018-07-30 12:49:31 -0700

    new url path

--- fitness_app_project/fitness_app/urls.py
+++ fitness_app_project/fitness_app/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  ############## LOG IN ###############
-  path('', views.index, name="login"),
-  ############# HOMEPAGE ##############
-  path('/homepage', views.homepage, name="homepage"),
-  ############# PROFILE ###############
-  path('user/<username>/', views.profile, name='profile'),
+    ############## LOG IN ###############
+    path("", views.index, name="login"),
+    ############# HOMEPAGE ##############
+    path("homepage/", views.homepage, name="homepage"),
+    ############# PROFILE ###############
+    path("user/<username>/", views.profile, name="profile"),
+    ############# SPECIFIC FOOD ###############
+    path("food/", views.food_view, name="food_view"),
 ]
