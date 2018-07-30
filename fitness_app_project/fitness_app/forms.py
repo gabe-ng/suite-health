e4823f47fb72e895147a742911922f2aa5b35b57
Riley <RP2@users.noreply.github.com>
2018-07-30 10:39:37 -0700

    login signup

--- fitness_app_project/fitness_app/forms.py
+++ fitness_app_project/fitness_app/forms.py
@@ -4,3 +4,7 @@ class LoginForm(forms.Form):
     username = forms.CharField(label="User Name", max_length=64)
     password = forms.CharField(widget=forms.PasswordInput())
 
+class SignupForm(forms.Form):
+    username = forms.CharField(label="User Name", max_length=64)
+    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput())
--- fitness_app_project/fitness_app/urls.py
+++ fitness_app_project/fitness_app/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
   ############## LOG IN ###############
   path('', views.login_view, name="login"),
+  path('signup/', views.signup_view, name="signup"),
   path('logout/', views.logout_view, name="logout"),
   ############# HOMEPAGE ##############
-  path('homepage', views.homepage, name="homepage"),
+  path('homepage/', views.homepage, name="homepage"),
   ############# PROFILE ###############
   path('user/<username>/', views.profile, name='profile'),
 ]
