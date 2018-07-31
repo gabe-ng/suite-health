03400f801b9d8ae74e4ff2cae617bfa215156f70
Gabriel Ng <gabrielng15@gmail.com>
2018-07-31 16:17:59 -0700

    removed comments from homepage js

--- fitness_app_project/fitness_app/static/js/homepage.js
+++ fitness_app_project/fitness_app/static/js/homepage.js
@@ -1,8 +1,3 @@
-// sample url if a user searched for 'red apple'. the '%20' represents a space.
-// 'https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id={your app_id}&app_key={your app_key}'
-// this copy and paste the below URL into Postman if you want to have a look at the responses.
-// 'https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=2d7d9644&app_key=8e911eeff3b68f04eafd1fffeaf16401'
-
 let muscles = {
   1: "Biceps Long Head",
   2: "Deltiods",
