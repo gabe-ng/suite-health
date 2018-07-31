9c87e3af31509f5758395dc56e8696875ff1adad
t10chap <tevinrawls@gmail.com>
2018-07-31 13:42:40 -0700

    Fixed error

--- fitness_app_project/fitness_app/static/js/homepage.js
+++ fitness_app_project/fitness_app/static/js/homepage.js
@@ -82,28 +82,10 @@ const test = () => {
   });
 };
 
-<<<<<<< HEAD
-// const saveWorkout = () => {
-//     let
-//     $.ajax({
-//         method: "POST",
-//         url: `api/workout//save`
-//     })
-// }
-||||||| merged common ancestors
 const saveWorkout = () => {
-    let 
     $.ajax({
         method: "POST",
-        url: `api/workout//save`
-    })
-}
-=======
-const saveWorkout = () => {
-    let
-    $.ajax({
-        method: "POST",
-        url: `api/workout//save`
-    })
-}
->>>>>>> 237cbac4afe6f2a42024b0b3dcdc40948bf022af
+        url: `api/workout//save`,
+        // success:
+    });
+  }
